670cc0667339c3ba23162042b034ca6124a53f0e
Armands Petrikas <armandspetrikas@gmail.com>
2022-06-03 15:13:04 +0300

    Finished Project

---
+++ assets/js/theme.js
@@ -0,0 +1,38 @@
+window.onload = function () {
+
+// // Burger menu 
+const menu_btn = document.querySelector('.hamburger');
+const mobile_menu = document.querySelector('.mobile-nav');
+const header = document.querySelector('.header');
+
+	menu_btn.addEventListener('click', function () {
+		menu_btn.classList.toggle('is-active');
+		mobile_menu.classList.toggle('active');
+		header.classList.toggle('sticky');
+	});
+
+// Scroll Up 
+document.addEventListener("scroll", scrolling);
+
+let scrollUpButton = document.querySelector("#scrollUpButton");
+
+function scrolling() {
+  let scrollableHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  let GOLDEN_RATIO = 0.3;
+
+  if ((document.documentElement.scrollTop / scrollableHeight ) > GOLDEN_RATIO) {
+    scrollUpButton.style.display = "block";
+  } else {
+    scrollUpButton.style.display = "none";
+  }
+}
+
+scrollUpButton.addEventListener("click", scrollToTop);
+
+  function scrollToTop() {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    })
+  }
+}
